5455593264583d6ca3fc6b1d6c362058a39a7eb9
mehdi7033 <mehdi7033@yahoo.com>
2015-08-24 16:37:25 +0430

    1

--- Section_controller/Properties/AssemblyInfo.cs
+++ Section_controller/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Section_controller")]
+[assembly: AssemblyTitle("Section_Controller")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Section_controller")]
+[assembly: AssemblyProduct("Section_Controller")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
